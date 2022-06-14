9c3cfb365713200a08c8031f5147d38ccb47fe6e
Pastor-git <maciej.pastor@gmail.com>
2022-06-14 21:20:49 +0200

    5 test cases done

--- tests/chrome_tests/chr_test_case5.py
+++ tests/chrome_tests/chr_test_case5.py
@@ -13,6 +13,24 @@ class Test1(unittest.TestCase):
     def test_test1(self):
         driver=self.driver
 
+
+        LOGO = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/img")
+        HOME_PAGE_BUTTON = driver.find_element(By.XPATH, "/html/body/div[1]/nav/table/tr[1]/a/button")
+        REPORTS_PAGE_BUTTON = driver.find_element(By.XPATH, "/html/body/div[1]/nav/table/tr[2]/a/button")
+        ARTICLES_PAGE_BUTTON = driver.find_element(By.XPATH, "/html/body/div[1]/nav/table/tr[3]/a/button")
+
+        REPORTS_PAGE_BUTTON.click()
+        is_displayed = LOGO.is_displayed()
+        self.assertTrue(is_displayed)
+
+        ARTICLES_PAGE_BUTTON.click()
+        is_displayed = LOGO.is_displayed()
+        self.assertTrue(is_displayed)
+
+        HOME_PAGE_BUTTON.click()
+        is_displayed = LOGO.is_displayed()
+        self.assertTrue(is_displayed)
+
     def tearDown(self):
         self.driver.quit()
 
